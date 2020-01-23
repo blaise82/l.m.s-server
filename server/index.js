def2fc5ba4892fdd3be4576be26c10c5343c4abd
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-01-23 16:05:09 +0200

    chore(express): set up node
    - set up express
    - set up dotenv
    - set up eslint
    
    closes #170868357

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Server Is On'));
+
+app.use((req, res) => {
+  res.status(400).send({
+    status: 400,
+    error: 'Bad Request',
+  });
+});
+
+if (!module.parent) {
+  app.listen(process.env.PORT || 8080);
+}
