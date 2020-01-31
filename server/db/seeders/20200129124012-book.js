daf573bef1c9b9589921c94edcb086f14d2fe2f0
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-01-31 08:36:35 +0200

    feat(Book): Add edit a book [Finishes#170868988]

--- server/db/seeders/20200129124012-book.js
+++ server/db/seeders/20200129124012-book.js
@@ -9,7 +9,19 @@ module.exports = {
     status: 'available',
     createdAt: new Date(),
     updatedAt: new Date(),
-  }], {}),
+  },
+  {
+    isbnNumber: 'b70c27ce-80aa-40e2-98cf-eb5ebf734269',
+    sectionId: 'e80a1be0-428d-11ea-9434-ab08a1e03ed1',
+    bookName: 'Secrets of love',
+    author: 'Emmanuel',
+    description: 'This a music book to teach you all you need to know about music',
+    bookPrice: 500,
+    status: 'available',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  ], {}),
 
   down: (queryInterface) => queryInterface.bulkDelete('Books', null, {}),
 };
